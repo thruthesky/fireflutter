48680aebda63802f13d02e602ff49c380d65d7ed
JaeHo Song <thruthesky@gmail.com>
2022-09-07 18:44:31 +0900

    update on push

--- firebase/functions/tests/test.ts
+++ firebase/functions/tests/test.ts
@@ -4,6 +4,9 @@ export class Test {
   // one push token.
   static token =
     "d-RUY7gztE1wnheilIWUYC:APA91bE_owxgKAYy7808o5EFFSKhyle5jpv9-tcfX2_KPh1rgrzh58K4erUwO1mk7bea6FVoksyH7ouACZjr0kA_kYe0X8uUergnAeS85UEBL3u7CxEC4sg3jRXLhGu2FdRKnqfuV0Ya";
+
+  static jaehoSimulatorToken =
+    "cVdUO22hkEd-rhGvagjHMJ:APA91bG5656PPaKawRSIQsN_gfqUqTd4CFCxmF7RcdvTlwqHjejCVTcILZMnl_RepEpg6mJn2MTc4dBAncv7bMB4Y5cEkqVCXH3ybgObsPlcoOJAqsz1xxoe_qR57t3gaN6ssDhKAmP3";
   // three push tokens. two are real. one is fake.
   static tokens = ["fake-token"];
 
