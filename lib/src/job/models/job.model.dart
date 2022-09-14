70bf3e50e781d2c2b61f7bcc6da6f362e727f8d7
YinG17 <simbulanja.17@gmail.com>
2022-09-14 14:55:28 +0800

    job update

--- lib/src/job/models/job.model.dart
+++ lib/src/job/models/job.model.dart
@@ -160,45 +160,27 @@ class JobModel {
     return '''JobModel($toUpdate)''';
   }
 
+  /// Create or Update job post.
+  ///
+  /// No point checking and deduction when creating job post (Sept. 14, 2022)
+  ///
+  /// Admin can create many jobs.
+  /// User can only create 1 job, and update it.
   Future edit() async {
     /// TODO complete job edit
-    ///
-    /// Admin can create many jobs.
-    /// User can only create 1 job, and update it.
-
     if (id == '') {
-      // Check if user have sufficient points to create a job post.
-      final userPoint = await Point.getUserPoint(uid);
-      if (userPoint < Job.pointDeductionForCreation) {
-        throw ERROR_LACK_OF_POINT_ON_JOB_CREATE;
-      }
-
-      // return await FunctionsApi.instance.request(
-      //   'jobCreate',
-      //   data: toCreate,
-      //   addAuth: true,
-      // );
       return await Job.jobCol.add({
         ...toCreate,
         'createdAt': FieldValue.serverTimestamp(),
         'updatedAt': FieldValue.serverTimestamp(),
       });
-
-      // Deduct point to user and update history.
-
     } else {
       /// Check if user owns the job post.
       if (uid != UserService.instance.uid) throw ERROR_NOT_YOUR_JOB_POST;
-
-      // return await FunctionsApi.instance.request(
-      //   'jobUpdate',
-      //   data: toUpdate,
-      //   addAuth: true,
-      // );
-      return await Job.jobCol.doc(id).set({
+      return await Job.jobCol.doc(id).update({
         ...toUpdate,
         'updatedAt': FieldValue.serverTimestamp(),
-      }, SetOptions(merge: true));
+      });
     }
   }
 }
