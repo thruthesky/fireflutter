2bc2b985beeeb86ff6b17a31f9147e4993949cfa
JaeHo Song <thruthesky@gmail.com>
2022-09-30 16:00:03 +0800

    porting user public data

--- firebase/functions/src/functions/user.functions.ts
+++ firebase/functions/src/functions/user.functions.ts
@@ -31,9 +31,17 @@ export const setUsersPublicData = functions
 
       // 사용자 문서가 생성 또는 수정 되었나?
       const data: any = snapshot.after.data();
-      delete data.uid;
+
+      // UID 의 값을 그대로 둔다. FlutterFlow 에서는 필요하다.
+      // delete data.uid;
+
       delete data.email;
       delete data.phone_number;
-      data.hasPhoto = data.photoURL != null;
+
+      // 사진이 있으면 hasPhoto 를 true 로 설정한다.
+      // FirebaseAuth.User 에서는 photoURL 이고, FireFlutter.User 에서는 photoUrl 이고, FlutterFlow 에는 photo_url 이다.
+      // 주의, 아래 3 개의 필드 중 하나 만 사용되어야 한다. 그래야 아래의 코드가 올바로 수행된다.
+      const url = data.photoURL ?? data.photoUrl ?? data.photo_url ?? "";
+      data.hasPhoto = !!url;
       return Ref.publicDoc(context.params.uid).set(data);
     });
