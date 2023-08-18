f86f26a17478b7660586acdc6db7dd8e909791b1
Christian Bucal <withcenter-dev2@github.com>
2023-08-18 13:25:29 +0800

    updated categories tests

---
+++ firebase/firestore/tests/categories/categories-delete.spec.js
@@ -0,0 +1,46 @@
+const assert = require("assert");
+const {
+    db,
+    a,
+    b,
+    c,
+    d,
+    categoriesColName,
+    admin,
+} = require("./../setup");
+
+// load firebase-functions-test SDK
+const firebase = require("@firebase/testing");
+
+describe("Categories Delete Test", () => {
+    it("Admin Cannot delete category - success", async () => {
+
+        // Prepare
+        const categoryRef = await admin().collection(categoriesColName).add({
+            'name': 'Test',
+            'createdAt': firebase.firestore.FieldValue.serverTimestamp(),
+            'updatedAt': firebase.firestore.FieldValue.serverTimestamp(),
+            'addedBy': 'test-uid-admin',
+        });
+
+        // Admin tries to delete the category - succeeds (for confirmation)
+        await firebase.assertSucceeds(
+            admin().collection(categoriesColName).doc(categoryRef.id).delete()
+        );
+    });
+    it("Non-Admin CANNOT delete category - failure", async () => {
+
+        // Prepare
+        const categoryRef = await admin().collection(categoriesColName).add({
+            'name': 'Test',
+            'createdAt': firebase.firestore.FieldValue.serverTimestamp(),
+            'updatedAt': firebase.firestore.FieldValue.serverTimestamp(),
+            'addedBy': 'test-uid-admin',
+        });
+
+        // Non-Admin tries to delete the category - failure
+        await firebase.assertFails(
+            db(a).collection(categoriesColName).doc(categoryRef.id).delete()
+        );
+    });
+});
---
+++ firebase/firestore/tests/categories/categories-update.spec.js
@@ -0,0 +1,52 @@
+const assert = require("assert");
+const {
+    db,
+    a,
+    b,
+    c,
+    d,
+    categoriesColName,
+    admin,
+} = require("./../setup");
+
+// load firebase-functions-test SDK
+const firebase = require("@firebase/testing");
+
+describe("Categories Update Test", () => {
+    it("Admin can update category - success", async () => {
+
+        // Prepare
+        const categoryRef = await admin().collection(categoriesColName).add({
+            'name': 'Test',
+            'createdAt': firebase.firestore.FieldValue.serverTimestamp(),
+            'updatedAt': firebase.firestore.FieldValue.serverTimestamp(),
+            'addedBy': 'test-uid-admin',
+        });
+
+        // Admin Updates the category
+        await firebase.assertSucceeds(
+            admin().collection(categoriesColName).doc(categoryRef.id).update({
+                'name': 'updated Name',
+                'description': 'updated description',
+            })
+        );
+    });
+    it("Non-Admin CANNOT add category - failure", async () => {
+
+        // prepare
+        const categoryRef = await admin().collection(categoriesColName).add({
+            'name': 'Test',
+            'createdAt': firebase.firestore.FieldValue.serverTimestamp(),
+            'updatedAt': firebase.firestore.FieldValue.serverTimestamp(),
+            'addedBy': 'test-uid-admin',
+        });
+
+        // Non Admin tried to update category - fail
+        await firebase.assertFails(
+            db(a).collection(categoriesColName).doc(categoryRef.id).update({
+                'name': 'updated Name',
+                'description': 'updated description',
+            })
+        );
+    });
+});
