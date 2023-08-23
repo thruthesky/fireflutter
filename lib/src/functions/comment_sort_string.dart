f1c83f25faeb32349f130c76e91e0a0db58ff01c
JaeHo Song <thruthesky@gmail.com>
2023-08-23 18:22:04 +0800

    adding sort functions

--- lib/src/functions/comment_sort_string.dart
+++ lib/src/functions/comment_sort_string.dart
@@ -1,13 +1,37 @@
+/// [sortCharLenth] is the length of each block in the sort string.
+/// Each block is a base32 number. If it is 2, then it supports up to
+/// 32^2 = 1024 comments in that depth.
+/// If it is 3, then it supports up to 32^4 = 1,048,576 comments in that depth.
+/// If the number of comments exceeds, then it will start with 0.
+const int sortCharLenth = 4;
+
+/// [sortBlocks] is the number of blocks in the sort string. If it's 10,
+/// It will support 10 depth of comments.
+/// if the [sortCharLength] is 4, then it will support up to 1,048,576 comments
+/// on each depth.
+const int sortBlocks = 10;
+
+/// Return a string which order the comments
+///
+/// [sortString] is the sort string of the comment. If it is null, it will be
+/// generated. If it is not null, it will be increased by 1 on its depth.
+/// [sortString] can be set to null for the first comment of a post.
+///
+/// [depth] is the depth of the comment. It starts with 0.
 ///
 ///
+/// @param sortString a sort string of last sibling comment. If the sibiling comments are not exist, then it must be the parents sort string.
+/// If there is parent(or parentId) of the comment, then return the sort of last first-level-comment's sort.
+/// @param depth parents depth.
+/// @returns
 String generateCommentSort({String? sortString, int depth = 0}) {
-  const int charLength = 3;
-  const int blocks = 8;
-  sortString = fillBlocksIfEmpty(order: sortString, blocks: blocks);
-  if (depth >= blocks) return sortString;
-  final List<String> parts = splitBy(inputString: sortString, len: charLength);
+  //
+  sortString = fillBlocksIfEmpty(order: sortString, blocks: sortBlocks);
+  // exceed the limit of blocks?
+  if (depth >= sortBlocks) return sortString;
+  final List<String> parts = splitBy(inputString: sortString, len: sortCharLenth);
   final String base32Str = parts[depth];
-  parts[depth] = increaseBase32By(str: base32Str).padLeft(charLength, '0');
+  parts[depth] = increaseBase32By(str: base32Str).padLeft(sortCharLenth, '0');
   sortString = parts.join('');
   return sortString;
 }
@@ -17,6 +41,10 @@ String increaseBase32By({required String str, int by = 1}) {
   return numToBase32(num: num);
 }
 
+/// Returns a string which order the comments
+///
+/// @param str string of base32
+/// @returns number
 int base32ToNum({required String str}) {
   if (str.isEmpty) return 0;
   const base32Chars = '0123456789ABCDEFGHIJKLMNOPQRSTUV';
@@ -33,6 +61,10 @@ int base32ToNum({required String str}) {
   return num;
 }
 
+/// Returns a string which order the comments
+///
+/// @param str string of base32
+/// @returns number
 String numToBase32({int? num}) {
   if (num == null) return '0';
   const base32Chars = '0123456789ABCDEFGHIJKLMNOPQRSTUV';
@@ -48,10 +80,13 @@ String numToBase32({int? num}) {
   return result;
 }
 
+/// Returns a string which order the comments
+///
+/// @param inputString input string
+/// @param len length of each part
+/// @returns array of string
 List<String> splitBy({required String inputString, int? len}) {
-  // TODO make it in a Config
-  const int charLength = 3;
-  len ??= charLength;
+  len ??= sortCharLenth;
   List<String> result = [];
   inputString = inputString.trim();
 
@@ -61,16 +96,10 @@ List<String> splitBy({required String inputString, int? len}) {
   return result;
 }
 
+/// Fills the sort string with 0 if it is empty.
+///
+/// [order] is the sort stirng. if it has value, then it will be returned as it
+/// is. If it is null, then it will be filled with 0.
 String fillBlocksIfEmpty({String? order, int blocks = 8}) {
-  // TODO make it in a config
-  const int charLength = 3;
-  if (order == null) {
-    order = List.filled(blocks, '0'.padLeft(charLength, '0')).join("");
-  } else {
-    order = order.trim();
-    if (order.isEmpty) {
-      order = List.filled(blocks, '0'.padLeft(charLength, '0')).join("");
-    }
-  }
-  return order;
+  return order ?? List.filled(blocks, ''.padLeft(sortCharLenth, '0')).join("");
 }
