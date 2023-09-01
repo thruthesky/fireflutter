84fe75b26e8e4395f6c4fa7a6f3240e7daac4987
JaeHo Song <thruthesky@gmail.com>
2023-09-01 10:35:20 +0800

    update on package

--- firebase/functions/.eslintrc.js
+++ firebase/functions/.eslintrc.js
@@ -20,14 +20,11 @@ module.exports = {
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    indent: ["error", 2],
     //
     "import/export": 0,
     "require-jsdoc": 0,
