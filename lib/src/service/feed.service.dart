535636ce77a9357aaba51efc38348dfe4dc40077
Christian Bucal <withcenter-dev2@github.com>
2023-09-01 14:04:17 +0800

    update feeds, delete feeds

--- lib/src/service/feed.service.dart
+++ lib/src/service/feed.service.dart
@@ -14,7 +14,6 @@ class FeedService with FirebaseHelper {
     if (re) {
       // get last 20 posts and save it under rtdb
       final posts = await PostService.instance.gets(uid: otherUid, limit: 20);
-
       for (final post in posts) {
         rtdb.ref('feeds').child(my.uid).child(post.id).set({
           'uid': post.uid,
@@ -30,16 +29,38 @@ class FeedService with FirebaseHelper {
         }
       });
     }
-
     return re;
   }
 
-  Future<List<Post>> getAllByMinusDate() async {
-    final feeds = await rtdb.ref('feeds').child(my.uid).orderByChild('createdAt').once();
-    List<Future<Post>> posts = [];
-    for (final feed in feeds.snapshot.children) {
-      posts.add(Post.get((feed.value as Map)['postId']));
+  // TODO Will be removed
+  // Future<List<Post>> getAllByMinusDate() async {
+  //   final feeds = await rtdb.ref('feeds').child(my.uid).orderByChild('createdAt').once();
+  //   List<Future<Post>> posts = [];
+  //   for (final feed in feeds.snapshot.children) {
+  //     posts.add(Post.get((feed.value as Map)['postId']));
+  //   }
+  //   return await Future.wait(posts);
+  // }
+
+  /// Adding posts into the feeds of the followers.
+  Future<List<void>> updateFeeds({required List<String> followers, required Post post}) async {
+    List<Future> feedUpdates = [];
+    for (String followerUid in followers) {
+      feedUpdates.add(rtdb.ref('feeds').child(followerUid).child(post.uid).set({
+        'uid': post.uid,
+        'postId': post.id,
+        'createdAt': 0 - post.createdAt.millisecondsSinceEpoch,
+      }));
     }
-    return await Future.wait(posts);
+    return await Future.wait(feedUpdates);
+  }
+
+  /// Used for updating RTDB feeds to followers' feed
+  Future deleteFeeds({required List<String> followers, required Post post}) async {
+    List<Future> feedDeletes = [];
+    for (String followerUid in followers) {
+      feedDeletes.add(rtdb.ref('feeds').child(followerUid).child(post.uid).remove());
+    }
+    return await Future.wait(feedDeletes);
   }
 }
