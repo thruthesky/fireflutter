f4f5b3ceb1026a1286d05e4e34426226785f44e9
JaeHo Song <thruthesky@gmail.com>
2023-09-08 16:04:33 +0800

    fix post doc

--- lib/src/widget/post/post_doc.dart
+++ lib/src/widget/post/post_doc.dart
@@ -3,19 +3,25 @@ import 'package:fireflutter/fireflutter.dart';
 import 'package:flutter/material.dart';
 
 class PostDoc extends StatelessWidget {
-  const PostDoc({super.key, required this.post, required this.builder});
+  const PostDoc({
+    super.key,
+    required this.postId,
+    this.post,
+    required this.builder,
+  });
 
-  final Post post;
+  final String postId;
+  final Post? post;
   final Widget Function(Post post) builder;
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<DocumentSnapshot>(
-      stream: Post.doc(post.id).snapshots(),
+      stream: Post.doc(postId).snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasError) return Text(snapshot.error.toString());
         if (snapshot.hasData) return builder(Post.fromDocumentSnapshot(snapshot.data!));
-        return builder(post);
+        return post != null ? builder(post!) : const Center(child: CircularProgressIndicator());
       },
     );
   }
