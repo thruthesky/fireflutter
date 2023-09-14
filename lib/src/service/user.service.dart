942e9020db8dbf12032635b2ff0489ce72cde48e
JaeHo Song <thruthesky@gmail.com>
2023-09-14 20:25:17 +0800

    merge conflict

--- lib/src/service/user.service.dart
+++ lib/src/service/user.service.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:fireflutter/fireflutter.dart';
-import 'package:fireflutter/src/enum/protocol.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
