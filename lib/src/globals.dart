a107dbebde164465931443dd5abb7e30ae36a798
lancelynyrd <pinedaclp@gmail.com>
2023-09-21 16:23:50 +0800

    update on the admin screen messaging

--- lib/src/globals.dart
+++ lib/src/globals.dart
@@ -1,7 +1,20 @@
+/// double sizeXxs = 4
 const double sizeXxs = 4;
+
+/// double sizeXs = 8
 const double sizeXs = 8;
+
+/// double sizeSm = 16
 const double sizeSm = 16;
+
+/// double sizeMd = 24
 const double sizeMd = 24;
+
+/// double sizeLg = 32
 const double sizeLg = 32;
+
+/// double sizeXl = 40
 const double sizeXl = 40;
+
+/// double sizeXxl = 48
 const double sizeXxl = 48;
