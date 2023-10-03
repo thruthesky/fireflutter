ca8946da3cf43a977a2c8ce6c900a5a81b321561
Christian Bucal <withcenter-dev2@github.com>
2023-10-03 20:48:38 +0800

    added get birthday and remove some notes in problems in console

--- lib/src/model/user/user.dart
+++ lib/src/model/user/user.dart
@@ -87,6 +87,12 @@ class User {
   /// LocalSttroage 에 캐시된 경우, 현재 시간, 아니면, Firestore 서버 시간
   DateTime get createdAt => data['createdAt'] is Timestamp ? (data['createdAt'] as Timestamp).toDate() : DateTime.now();
 
+  /// Gets the birthdate using the birthYear, birthMonth, and birthDay of the user
+  DateTime get birthdate => DateTime(birthYear, birthMonth, birthDay);
+
+  /// Gets the age of the user
+  int get age => DateTime.now().difference(birthdate).inDays ~/ 365;
+
   /// Set this to true when the user has completed the profile.
   /// This should be set when the user submit the profile form.
   ///
