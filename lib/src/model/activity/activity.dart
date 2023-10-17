589e77155807778e3b16237faf10c229c954707c
lancelynyrd <pinedaclp@gmail.com>
2023-10-17 17:11:55 +0800

    update on the activity model

--- lib/src/model/activity/activity.dart
+++ lib/src/model/activity/activity.dart
@@ -65,6 +65,7 @@ class Activity {
     await re.set(data);
     final act = await activityLogRef.child(re.key!).get();
     re.update({'reverseCreatedAt': (act.value as dynamic)['createdAt'] * -1});
+    activityUserLogRef().push().set({...data, 'reverseCreatedAt': (act.value as dynamic)['createdAt'] * -1});
   }
 
   factory Activity.fromJson(Map<String, dynamic> json) {
