45037cfd7cc54ecae545ae22c505a6873dca7c76
Christian Bucal <withcenter-dev2@github.com>
2023-10-23 17:04:10 +0800

    post title for blocked

--- lib/src/widget/post/post.title.dart
+++ lib/src/widget/post/post.title.dart
@@ -1,1 +1,21 @@
-// TODO post title
+import 'package:fireflutter/fireflutter.dart';
+import 'package:flutter/material.dart';
+
+class PostTitle extends StatelessWidget {
+  const PostTitle({
+    super.key,
+    required this.post,
+    this.style,
+  });
+
+  final Post post;
+  final TextStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      my?.hasBlocked(post.uid) ?? false ? tr.blocked : post.title.replaceAll("\n", " "),
+      style: style,
+    );
+  }
+}
