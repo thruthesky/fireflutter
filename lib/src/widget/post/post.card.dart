b002f8a9c5a63e216dad4f698eff2557bd600e72
Christian Bucal <withcenter-dev2@github.com>
2023-10-27 09:00:01 +0800

    commit post card blink resolution

--- lib/src/widget/post/post.card.dart
+++ lib/src/widget/post/post.card.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fireflutter/fireflutter.dart';
