6bcb6e2f00b42d6f3d68adbf4c1afa5e8fa7c858
JaeHo Song <thruthesky@gmail.com>
2023-10-29 13:53:29 +0900

    comment

--- lib/src/functions/comment_sort_string.dart
+++ lib/src/functions/comment_sort_string.dart
@@ -1,6 +1,3 @@
-/// depth and maxDepth must not be more than 9
-const int maxDepth = 9;
-
 /// Gets the sort sortString string
 ///
 /// [sortString] is the sort string. It must come from the parent's comment.
@@ -24,16 +21,17 @@ String getCommentSortString({
   int? noOfComments,
   String? sortString,
 }) {
-  /// 맨 첫 번째 코멘트란, 글에 최초로 작성되는 코멘트. 1번째 코멘트.
+  /// depth and maxDepth must not be more than 9
+  const int maxDepth = 9;
+
+  /// 맨 첫 번째 코멘트란, 글에 최초로 작성되는 코멘트. 1번째 코멘트. 해당 글에 하나 뿐.
   /// 첫번째 레벨 코멘트란, 글 바로 아래에 작성되는 코멘트로, 부모 코멘트가 없는 코멘트이다. 이 경우 depth=0 이다.
-  /// noOfComments 는 부모의 noOfComments 이다. 첫번째 레벨 코멘트는 글의 noOfComments, 하위 코멘트는 상위(부모) 코멘트의 noOfComments. (예: 부모 comment 가 있으면, 부모 comment 의 것을 사용. 하니면 글의 것을 사용).
-  /// 참고, 맨 첫번째 코멘트를 작성할 때는 noOfComments 값을 그냥 0의 값(또는 NULL)을 입력하면 된다.
+  /// noOfComments 는 글의 noOfComments 이다. 참고, 맨 첫번째 코멘트를 작성할 때는 noOfComments 값을 그냥 0의 값(또는 NULL)을 입력하면 된다.
   /// [sortString] 는 부모 코멘트의 sortString string 이다. 첫번째 레벨 코멘트의 경우, 빈 NULL (또는 빈문자열)을 입력하면 된다.
   /// [depth] 는 부모 코멘트의 depth 이다. depth 를 부모의 것을 그대로 입력하면 된다. 함수로 전달하기 전에 +1 을 할 필요 없다.
   /// ( 예: 부모 코멘트가 존재하면, 부모의 것을(기본 값은 0) 아니면, 글의 것을 사용(기본 값은 0) )
-  /// 주의: 여기서 depth 는 sortString 구분 문자열의 칸을 말하는 것이며 0 부터 시작한다.
-  /// 하지만, 실제 코멘트 문서에 저장되는 depth 는 1부터 시작해야 한다.
-  /// (예를 들면, depth 값을 1 증가 시키기 위해서, +1 증가시키는 함수(IncreaseInteger)를 써야 하는데, 첫번째 레벨의 경우, IncreaseInteger 함수에 0(NULL)을 지정하면 +1을 해서, 1 값이 리턴된다. 그 값을 comment 문서의 depth 에 저장하므로, 자연스럽게 1 부터 시작하는 것이다. 또는 첫번째 레벨의 코멘트는 그냥 depth=1 로 지정하면 된다. 그리고 사실은 0으로 시작하든 1로 시작하던, UI 랜더링 할 때, depth 만 잘 표현하면 된다. 개발방법: 부모가 있으면 부모depth+1 아니면 1.)
+  /// 참고, 여기서 depth 는 sortString 구분 문자열의 칸을 말하는 것으로, 그냥 부모의 depth 만 그대로 입력하면 된다.
+  /// 단, 새로운 코멘트를 저장 할 때에는 부모 depth 에 +1 을 해서 저장해야 한다.
   noOfComments ??= 0;
   sortString ??= "";
   if (sortString == "" || depth == 0) {
@@ -43,8 +41,7 @@ String getCommentSortString({
     if (depth > maxDepth) depth = maxDepth;
     List<String> parts = sortString.split('.');
     String block = parts[depth];
-    int computed =
-        int.parse(block) + noOfComments + 1; // 처음이 0일 수 있다. 0이면, 부모와 같아서 안됨.
+    int computed = int.parse(block) + noOfComments + 1; // 처음이 0일 수 있다. 0이면, 부모와 같아서 안됨.
     parts[depth] = computed.toString();
     sortString = parts.join('.');
     return sortString;
