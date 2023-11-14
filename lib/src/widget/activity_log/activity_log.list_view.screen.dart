64a46729874f2c86dfce3a0692af99ce15f2f9b4
lancelynyrd <pinedaclp@gmail.com>
2023-11-14 13:47:45 +0800

    adding my time line

--- lib/src/widget/activity_log/activity_log.list_view.screen.dart
+++ lib/src/widget/activity_log/activity_log.list_view.screen.dart
@@ -5,11 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class ActivityLogListViewScreen extends StatefulWidget {
-  const ActivityLogListViewScreen({super.key});
+  const ActivityLogListViewScreen({super.key, this.myActivity = false});
+
+  final bool myActivity;
 
   @override
-  State<ActivityLogListViewScreen> createState() =>
-      _ActivityLogListViewScreenState();
+  State<ActivityLogListViewScreen> createState() => _ActivityLogListViewScreenState();
 }
 
 class _ActivityLogListViewScreenState extends State<ActivityLogListViewScreen> {
@@ -18,7 +19,9 @@ class _ActivityLogListViewScreenState extends State<ActivityLogListViewScreen> {
   get query {
     Query q = activityLogCol.orderBy('createdAt', descending: true);
 
-    if (search.text.isNotEmpty) {
+    if (widget.myActivity == true) {
+      q = q.where('uid', isEqualTo: myUid);
+    } else if (search.text.isNotEmpty) {
       q = q.where('uid', isEqualTo: search.text);
     }
 
@@ -35,10 +38,6 @@ class _ActivityLogListViewScreenState extends State<ActivityLogListViewScreen> {
   @override
   void initState() {
     super.initState();
-
-    // FirebaseFirestore.instance.collection('activity_logs').count().get().then((value) => dog('count; ${value.count}'));
-
-    // query.count().get().then((value) => dog('count; ${value.count}'));
   }
 
   @override
@@ -59,7 +58,114 @@ class _ActivityLogListViewScreenState extends State<ActivityLogListViewScreen> {
             },
           )
         ],
-        bottom: userSearch,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(widget.myActivity == true ? 32 : 90),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (widget.myActivity == false)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: sizeSm),
+                    child: Stack(
+                      children: [
+                        TextField(
+                          controller: search,
+                          style: const TextStyle(fontSize: 10),
+                          decoration: const InputDecoration(
+                            hintText: 'Enter uid',
+                            helperMaxLines: 2,
+                            helperStyle: TextStyle(fontSize: 10),
+                          ),
+                          onSubmitted: (v) => setState(() {}),
+                        ),
+                        Positioned(
+                          right: 0,
+                          top: 4,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              if (search.text.isNotEmpty)
+                                IconButton(
+                                  onPressed: () {
+                                    search.text = '';
+                                    setState(() {});
+                                  },
+                                  icon: const Icon(Icons.close_outlined),
+                                ),
+                              IconButton(
+                                visualDensity: const VisualDensity(horizontal: 2, vertical: -2),
+                                onPressed: () {
+                                  AdminService.instance.showUserSearchDialog(
+                                    context,
+                                    field: 'name',
+                                    onTap: (user) async {
+                                      search.text = user.uid;
+                                      Navigator.of(context).pop();
+                                      setState(() {});
+                                    },
+                                  );
+                                },
+                                icon: const Icon(Icons.search),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 22,
+                      ),
+                      ...['all', ...ActivityLogService.instance.activityLogTypes]
+                          .map(
+                            (type) => GestureDetector(
+                              behavior: HitTestBehavior.opaque,
+                              onTap: () {
+                                setState(() {
+                                  searchType = type;
+                                });
+                              },
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Radio(
+                                    value: type,
+                                    visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
+                                    groupValue: searchType,
+                                    onChanged: (String? value) {
+                                      if (value != null) {
+                                        setState(() {
+                                          searchType = value;
+                                        });
+                                      }
+                                    },
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: sizeXs),
+                                    child: Text(
+                                      toBeginningOfSentenceCase(type)!,
+                                      style: const TextStyle(fontSize: 12),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
       body: FirestoreListView(
         // itemExtent: 350,
@@ -78,110 +184,4 @@ class _ActivityLogListViewScreenState extends State<ActivityLogListViewScreen> {
       ),
     );
   }
-
-  PreferredSize get userSearch => PreferredSize(
-        preferredSize: const Size.fromHeight(90),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: sizeSm),
-              child: Stack(
-                children: [
-                  TextField(
-                    controller: search,
-                    style: const TextStyle(fontSize: 10),
-                    decoration: const InputDecoration(
-                      hintText: 'Enter uid',
-                      helperMaxLines: 2,
-                      helperStyle: TextStyle(fontSize: 10),
-                    ),
-                    onSubmitted: (v) => setState(() {}),
-                  ),
-                  Positioned(
-                    right: 0,
-                    top: 4,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        if (search.text.isNotEmpty)
-                          IconButton(
-                            onPressed: () {
-                              search.text = '';
-                              setState(() {});
-                            },
-                            icon: const Icon(Icons.close_outlined),
-                          ),
-                        IconButton(
-                          visualDensity:
-                              const VisualDensity(horizontal: 2, vertical: -2),
-                          onPressed: () {
-                            AdminService.instance.showUserSearchDialog(
-                              context,
-                              field: 'name',
-                              onTap: (user) async {
-                                search.text = user.uid;
-                                Navigator.of(context).pop();
-                                setState(() {});
-                              },
-                            );
-                          },
-                          icon: const Icon(Icons.search),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  const SizedBox(
-                    width: sizeXs,
-                  ),
-                  ...['all', ...ActivityLogService.instance.activityLogTypes]
-                      .map(
-                        (type) => InkWell(
-                          onTap: () {
-                            setState(() {
-                              searchType = type;
-                            });
-                          },
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Radio(
-                                value: type,
-                                visualDensity: const VisualDensity(
-                                    horizontal: -4, vertical: -4),
-                                groupValue: searchType,
-                                onChanged: (String? value) {
-                                  if (value != null) {
-                                    setState(() {
-                                      searchType = value;
-                                    });
-                                  }
-                                },
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: sizeXs),
-                                child: Text(
-                                  toBeginningOfSentenceCase(type)!,
-                                  style: const TextStyle(fontSize: 12),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                      .toList(),
-                ],
-              ),
-            )
-          ],
-        ),
-      );
 }
