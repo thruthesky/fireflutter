ba888b83e20a7ba3d1b18f2bd617ee623b4062f8
Christian Bucal <withcenter-dev2@github.com>
2023-11-15 10:30:42 +0800

    commiting chat box ui

--- lib/src/widget/chat/room/chat.room.message_box.upload_button.dart
+++ lib/src/widget/chat/room/chat.room.message_box.upload_button.dart
@@ -2,18 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:fireflutter/fireflutter.dart';
 
 class ChatRoomMessageBoxUploadButton extends StatelessWidget {
-  const ChatRoomMessageBoxUploadButton({super.key, this.room, this.onProgress});
+  const ChatRoomMessageBoxUploadButton({
+    super.key,
+    this.room,
+    this.onProgress,
+    this.visualDensity,
+    this.icon,
+  });
 
   final Room? room;
   final Function(double?)? onProgress;
+  final VisualDensity? visualDensity;
+  final Widget? icon;
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: const Icon(
-        Icons.camera_alt,
-        size: 28,
-      ),
+      visualDensity: visualDensity ?? VisualDensity.standard,
+      icon: icon ??
+          Icon(
+            Icons.camera_alt,
+            // size: 28,
+            size: 24,
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
       padding: EdgeInsets.zero,
       onPressed: () async {
         if (room == null) {
