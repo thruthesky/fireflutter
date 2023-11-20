f100fe3e62bcf49caa992fc08757da19ae103269
Noe <withcenter.dev4@gmail.com>
2023-11-20 12:12:19 +0800

    remove tr.varibale for translation

--- lib/src/service/activity_log.service.dart
+++ lib/src/service/activity_log.service.dart
@@ -8,7 +8,7 @@ class ActivityLogService {
 
   bool enableActivityLog = false;
 
-  List<String> activityLogTypes = [tr.labelUser, tr.labelPost, tr.labelComment];
+  List<String> activityLogTypes = ['user', 'post', 'comment'];
 
   ActivityLogCustomize customize = ActivityLogCustomize();
 
