7d76c16d415efbe8f184324602e9060910e8ccd5
JaeHo Song <thruthesky@gmail.com>
2023-11-21 16:28:46 +0800

    update on user list

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jaehosong/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jaehosong/christian/fireflutter"
+export "FLUTTER_ROOT=/Users/thruthesky/bin/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/thruthesky/apps/flutter/packages/fireflutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.3.15"
