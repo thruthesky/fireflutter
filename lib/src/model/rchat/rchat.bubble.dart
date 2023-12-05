562d6013c7b5032ca8f4a46ce17dc589db6135f7
Noe <withcenter.dev4@gmail.com>
2023-12-05 10:32:51 +0800

    update on chat bubble darkmode

--- lib/src/model/rchat/rchat.bubble.dart
+++ lib/src/model/rchat/rchat.bubble.dart
@@ -42,7 +42,7 @@ class RChatBubble extends StatelessWidget {
                 color: message.mine ? Colors.amber.shade200 : Colors.grey.shade200,
                 borderRadius: borderRadius(),
               ),
-              child: Text(message.text ?? ''),
+              child: Text(message.text ?? '', style: const TextStyle(color: Colors.black)),
             ),
           // image
           if (message.url != null) cachedImage(context, message.url!),
