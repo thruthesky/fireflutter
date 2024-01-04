618234179f71027d3fc9c965d79e05212743a9a1
Noe <withcenter.dev4@gmail.com>
2024-01-04 18:08:59 +0800

    update on token

--- firebase/functions/src/test/send-message.ts
+++ firebase/functions/src/test/send-message.ts
@@ -11,7 +11,7 @@ console.log('Nothing shows up?');
     await MessagingService.sendNotificationToTokens([
         /// iPhone11ProMax
         "fVWDxKs1kEzxhtV9ElWh-5:APA91bE_rN_OBQF3KwAdqd6Ves18AnSrCovj3UQyoLHvRwp0--1BRyo9af8EDEWXEuzBneknEFFuWZ7Lq2VS-_MBRY9vbRrdXHEIAOtQ0GEkJgnaJqPYt7TQnXtci3s0hxn34MBOhwSK",
-        ///
-        "",
+        /// andriod with initail uid of W7a
+        "c9OPdjOoRtqCOXVG7HLpSI:APA91bFJ9VshAvx-mQ4JsIpFmkljnA4XZtE8LDw6JYtIWSJwSxnuJsHt0XtlHKy4wuRcttIzqPQckfAwX_baurPfiJuFFNS6ioD50X9ks5eeyi5Pl40vMWmCpNpgCVxg92CjRe5S51Ja",
     ], "Hello, from test", "How are you?");
 })();
