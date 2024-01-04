a0e7f0fbe2db92a792375565531a593300c7ceb1
JaeHo Song <thruthesky@gmail.com>
2024-01-04 15:51:06 +0800

    refactoring files

--- docs/install.md
+++ docs/install.md
@@ -10,9 +10,18 @@
 
 ## Install Cloud Functions
 
+Run the following to install `sendPushNotifications` cloud function.
+
+```sh
+% npm run deploy
+```
+
+And set the end point URL to `MessagingService.instance.init(sendPushNotificationsUrl: ..)`
 
 
 ## Admin
 
 
 - See [Admin Doc](admin.md)
+
+
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -7,13 +7,15 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const sendPushNotifications = onRequest((request, response) => {
+    logger.info("Hello logs!", { structuredData: true });
+
+    // getMessaging();
+    response.send(request.query);
+});
