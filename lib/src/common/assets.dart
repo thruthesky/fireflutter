aabd3316c5953a20287d018e6433f25b1031baaf
JaeHo Song <thruthesky@gmail.com>
2024-01-05 13:50:10 +0800

    anoynhmous phot

--- lib/src/common/assets.dart
+++ lib/src/common/assets.dart
@@ -1,6 +1,6 @@
 const String whiteUrl =
-    'https://raw.githubusercontent.com/thruthesky/fireship/main/assets/white.jpg';
+    'https://raw.githubusercontent.com/thruthesky/fireship/main/assets/white.jpg?raw=true';
 const String blackUrl =
-    'https://raw.githubusercontent.com/thruthesky/fireship/main/assets/black.jpg';
+    'https://raw.githubusercontent.com/thruthesky/fireship/main/assets/black.jpg?raw=true';
 const String anonymousUrl =
-    'https://raw.githubusercontent.com/thruthesky/fireship/main/assets/anonymous.jpg';
+    'https://raw.githubusercontent.com/thruthesky/fireship/main/assets/anonymous.jpg?raw=true';
