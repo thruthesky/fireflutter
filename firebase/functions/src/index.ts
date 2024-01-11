373359557ec186073051a8dfdc0d7ebf22f6a0ae
JaeHo Song <thruthesky@gmail.com>
2024-01-11 15:49:35 +0800

    fix idnex.ts error handling

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,8 @@
 
-import {onRequest} from "firebase-functions/v2/https";
-import {MessagingService} from "./messaging/messaging.service";
-import {initializeApp} from "firebase-admin/app";
-import {logger} from "firebase-functions/v1";
+import { onRequest } from "firebase-functions/v2/https";
+import { MessagingService } from "./messaging/messaging.service";
+import { initializeApp } from "firebase-admin/app";
+import { logger } from "firebase-functions/v1";
 
 
 // / initialize firebase app
@@ -19,7 +19,9 @@ export const sendPushNotifications = onRequest(async (request, response) => {
   } catch (e) {
     logger.error(e);
     if (e instanceof Error) {
-      response.send({error: e.message});
+      response.send({ error: e.message });
+    } else {
+      response.send({ error: "unknown error" });
     }
   }
 });
