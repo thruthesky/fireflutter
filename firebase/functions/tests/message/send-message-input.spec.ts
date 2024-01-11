43e340615b06fdc8116b9be0934918f7e78e9a5f
Noe <withcenter.dev4@gmail.com>
2024-01-11 11:32:28 +0800

    update on unit test

--- firebase/functions/tests/message/send-message-input.spec.ts
+++ firebase/functions/tests/message/send-message-input.spec.ts
@@ -12,41 +12,62 @@ if (admin.apps.length === 0) {
  * This test is not reliable because the tokens may be invalid after a while.
  */
 describe("Send message", () => {
-    it("Check invalid tokens", async () => {
+
+    it("Check if not an array of string", async () => {
         try {
             const res = await MessagingService.sendNotificationToTokens(
-                {
-                    tokens: [
-                        "e66JGEFqRWOuictuIH8pnk:APA91bEPzpJI1IzfWs-A1UO13Mly3YQU07kpQZyl5KVXYowKts_ILI6l624ZtSk2wljVaY62xXHJNFvLKvfCNvzUI9QjEygKPjC0NROBnKQ3P__LZU2d5fd2-jdlUosOwoViLnUEaADN",
-                        // TEST empty token
-                        "",
-                        // TEST invalid token
-                        "This-is-invalid-token",
-                        true as any,
-                        5 as any,
-                        0 as any,
-                    ]
-                } as any
+                {  
+                    tokens : 
+                    [ "fVWDxKs1kEzxhtV9ElWh-5:APA91bE_rN_OBQF3KwAdqd6Ves18AnSrCovj3UQyoLHvRwp0--1BRyo9af8EDEWXEuzBneknEFFuWZ7Lq2VS-_MBRY9vbRrdXHEIAOtQ0GEkJgnaJqPYt7TQnXtci3s0hxn34MBOhwSK",
+                      "",
+                      // TEST invalid token
+                      "This-is-invalid-token",
+                      0 as any
+                    ],
+                    title : 'Test Title',
+                    body:'Test Body',
+                }
             );
+            res['status'] = "passed"
             console.log("res", res);
         } catch (e) {
             assert((e as any).message == "tokens must be an array of string", "tokens must be an array of string");
+        }
+        
+    });
+    it("Check if Empty tokens", async () => {
+        try {
+            const res = await MessagingService.sendNotificationToTokens(
+                {tokens:[] as any, title:"hello", body:"hello",}
+            );
+            console.log("res", res);
+        } catch (e) {
+            assert((e as any).message == "tokens must not be empty", "tokens must be an array of string");
+        }
+        
+    });
+
+    it("Check Empty title", async () => {
+        try {
+            const res = await MessagingService.sendNotificationToTokens(
+                {tokens:['asd'] as any, title:"", body:"hello",}
+            );    res['status'] = "passed"
+            console.log("res", res);
+        } catch (e) {
+            assert((e as any).message == "title must not be empty", "Titile must not be empty");
         }
     });
 
-    it("Check invalid title", async () => {
+    it("Check Empty Body", async ()=>{
+       
         try {
             const res = await MessagingService.sendNotificationToTokens(
-                {
-                    tokens: [
-                        "e66QjEygKPjC0NROBnKQ3P__LZU2d5fd2-jdlUosOwoViLnUEaADN",
-                    ],
-                    title: '',
-                } as any,
+                {tokens:['some token'] as any, title:"hello", body:'',}
             );
+            res['status'] = "passed"
             console.log("res", res);
         } catch (e) {
-            assert((e as any).message == "tokens must be an array of string", "tokens must be an array of string");
+            assert((e as any).message == "body must not be empty", "Body must not be empty");
         }
-    });
+    })
 });
