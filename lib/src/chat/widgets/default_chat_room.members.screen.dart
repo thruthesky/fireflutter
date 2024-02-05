d6bf0bbd5a1905f96689cc5489970c43a46e6a5a
Christian Bucal <withcenter-dev2@github.com>
2024-02-05 13:23:17 +0800

    used FirebaseDatabaseQueryBuilder to get members

--- lib/src/chat/widgets/default_chat_room.members.screen.dart
+++ lib/src/chat/widgets/default_chat_room.members.screen.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_database/firebase_ui_database.dart';
 import 'package:fireship/fireship.dart';
 import 'package:fireship/src/chat/widgets/default_chat_room.member.dialog.dart';
 import 'package:flutter/material.dart';
@@ -11,48 +11,57 @@ class DefaultChatRoomMembersScreen extends StatelessWidget {
 
   final ChatRoomModel room;
 
-  /// A Getter to get a List of uids of the members of the room.
-  List<String> get members => (room.users ?? {})
-      .map((key, value) => MapEntry(key, value))
-      .keys
-      .toList();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Members'),
       ),
-      body: ListView.builder(
-        itemCount: members.length,
-        itemBuilder: (context, index) {
-          final member = UserModel.get(members[index]);
-          return GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () async {
-              final awaitUser = await member;
-              if (awaitUser == null) return;
-              if (!context.mounted) return;
-              showDialog(
-                context: context,
-                builder: (context) => DefaultChatRoomMemberDialog(
-                  room: room,
-                  member: awaitUser,
+      body: FirebaseDatabaseQueryBuilder(
+        query: ChatService.instance.roomsRef.child(room.id).child(Field.users),
+        pageSize: 50,
+        builder: (context, snapshot, _) {
+          if (snapshot.isFetching) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Text('Something went wrong! ${snapshot.error}');
+          }
+          if (snapshot.hasMore == false && snapshot.docs.isEmpty) {
+            return const Text('No members!');
+          }
+          return ListView.builder(
+            itemCount: snapshot.docs.length,
+            itemBuilder: (context, index) {
+              final member = UserModel.get(snapshot.docs[index].key!);
+              return GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () async {
+                  final awaitUser = await member;
+                  if (awaitUser == null) return;
+                  if (!context.mounted) return;
+                  showDialog(
+                    context: context,
+                    builder: (context) => DefaultChatRoomMemberDialog(
+                      room: room,
+                      member: awaitUser,
+                    ),
+                  );
+                },
+                child: SizedBox(
+                  height: 70,
+                  child: FutureBuilder(
+                    future: member,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return UserTile(user: snapshot.data as UserModel);
+                      }
+                      return const SizedBox.shrink();
+                    },
+                  ),
                 ),
               );
             },
-            child: SizedBox(
-              height: 70,
-              child: FutureBuilder(
-                future: member,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return UserTile(user: snapshot.data as UserModel);
-                  }
-                  return const SizedBox.shrink();
-                },
-              ),
-            ),
           );
         },
       ),
