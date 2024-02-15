beac5f49b497932402b4936dbc68324eb0114984
JaeHo Song <thruthesky@gmail.com>
2024-02-15 18:06:02 +0900

    update country pciker

--- docs/korean/country_picker.md
+++ docs/korean/country_picker.md
@@ -7,3 +7,39 @@
 
 
 
+
+## 국가 선택 예제
+
+```dart
+CountryPicker(
+    onChanged: (v) {
+        print(v);
+    },
+),
+```
+
+
+아래는 몇개 나라만 목록에 보여주는 예제이다.
+
+```dart
+CountryPicker(
+    filters: const ['KR', 'VN', 'TH', 'LA', 'MM'],
+    onChanged: (v) {
+        print(v);
+    },
+),
+```
+
+만약 보여주는 나라 개수가 적다면, search box 를 표시 하지 않을 수 있다.
+
+```dart
+CountryPicker(
+  filters: const ['KR', 'VN', 'TH', 'LA', 'MM'],
+  search: false,
+  onChanged: (v) {
+    print(v);
+  },
+),
+```
+
+아래와 같이 헤더를 직접 디자인 할 수 있다.
--- lib/src/common/country_picker/country_picker.dart
+++ lib/src/common/country_picker/country_picker.dart
@@ -1,17 +1,29 @@
 import 'package:fireship/fireship.dart';
 import 'package:flutter/material.dart';
 
-class CountryPicker extends StatelessWidget {
+class CountryPicker extends StatefulWidget {
   const CountryPicker({
     super.key,
     this.filters,
+    this.search = true,
+    this.headerBuilder,
+    this.labelBuilder,
     required this.onChanged,
   });
 
   /// List of country codes to show in the list.
   final List<String>? filters;
+  final bool search;
+  final Widget Function()? headerBuilder;
+  final Widget Function(Map<String, String>)? labelBuilder;
   final void Function(Map<String, String>) onChanged;
 
+  @override
+  State<CountryPicker> createState() => _CountryPickerState();
+}
+
+class _CountryPickerState extends State<CountryPicker> {
+  Map<String, String> selectedCountry = {};
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
@@ -20,11 +32,21 @@ class CountryPicker extends StatelessWidget {
           context: context,
           builder: (context) {
             return CountryPickerListView(
-                onChanged: onChanged, filters: filters);
+              search: widget.search,
+              onChanged: (v) {
+                setState(() {
+                  selectedCountry = v;
+                });
+                widget.onChanged(v);
+              },
+              headerBuilder: widget.headerBuilder,
+              filters: widget.filters,
+            );
           },
         );
       },
-      child: const Text('CountryPicker'),
+      child: widget.labelBuilder?.call(selectedCountry) ??
+          const Text('CountryPicker'),
     );
   }
 }
