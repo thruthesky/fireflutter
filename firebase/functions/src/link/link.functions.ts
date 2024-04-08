b8cc910e0bbb6dcf97d997851a0f9860b1c80f8e
JaeHo Song <thruthesky@gmail.com>
2024-04-08 11:30:28 +0800

    update

--- firebase/functions/src/link/link.functions.ts
+++ firebase/functions/src/link/link.functions.ts
@@ -2,31 +2,21 @@
 
 import * as functions from "firebase-functions";
 import * as express from "express";
-import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, DocumentSnapshot } from "firebase/firestore";
+import { LinkCredential } from "./link.interface";
 
 // Initialize Express app
 const app = express();
 // Set up Firebase Cloud Function
 export const link = functions.https.onRequest(app);
 
-const firebaseConfig = {
-    apiKey: "AIzaSyC8ME9ffn9KwEg01zrtzHgHdV80V0Qxj2c",
-    authDomain: "withcenter-meetup-3.firebaseapp.com",
-    databaseURL: "https://withcenter-meetup-3-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "withcenter-meetup-3",
-    storageBucket: "withcenter-meetup-3.appspot.com",
-    messagingSenderId: "103508765497",
-    appId: "1:103508765497:web:64cbdcc1f57acf8ddad6e8",
-  };
 
 /**
  * Returns the Document Snapshot
  * @param docId
  */
-async function getDeeplinkDoc( docId: string ): Promise<DocumentSnapshot> {
-    const firebaseApp = initializeApp(firebaseConfig);
-    const db = getFirestore(firebaseApp);
+async function getDeeplinkDoc(docId: string): Promise<DocumentSnapshot> {
+    const db = getFirestore();
     const docRef = doc(db, "_deeplink_", docId);
     const docSnap = await getDoc(docRef);
     return docSnap;
@@ -36,7 +26,9 @@ app.get("/.well-known/apple-app-site-association", async (req, res) => {
     const docSnaphot = await getDeeplinkDoc("apple");
     res.writeHead(200, { "Content-Type": "application/json" });
     if (docSnaphot.exists()) {
-        res.write(JSON.stringify(docSnaphot.data()["value"]));
+        const data = docSnaphot.data() as LinkCredential;
+
+        res.write(JSON.stringify(data.value));
     } else {
         // docSnap.data() will be undefined in this case
         res.write("Page not found!");
@@ -48,7 +40,7 @@ app.get("/.well-known/assetlinks.json", async (req, res) => {
     const docSnaphot = await getDeeplinkDoc("android");
     res.writeHead(200, { "Content-Type": "application/json" });
     if (docSnaphot.exists()) {
-        res.write(JSON.stringify(docSnaphot.data()["value"]));
+        res.write(JSON.stringify((docSnaphot.data() as LinkCredential).value));
     } else {
         // docSnap.data() will be undefined in this case
         res.write("Page not found!");
@@ -59,7 +51,7 @@ app.get("/.well-known/assetlinks.json", async (req, res) => {
 app.get("*", async (req, res) => {
     const docSnaphot = await getDeeplinkDoc("html");
     if (docSnaphot.exists()) {
-        const source = docSnaphot.data()["value"];
+        const source = (docSnaphot.data() as LinkCredential).value;
         // Return the webpage
         return res.send(source);
     }
---
+++ firebase/functions/src/link/link.interface.ts
@@ -0,0 +1,3 @@
+export interface LinkCredential {
+    value: string;
+}
