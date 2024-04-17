1217823f791df4c9249ab0f2af04af7722ffd4dd
Christian Bucal <withcenter-dev2@github.com>
2024-04-17 11:15:43 +0800

    app ID is added in HTML

--- firebase/functions/src/link/link.functions.ts
+++ firebase/functions/src/link/link.functions.ts
@@ -79,7 +79,7 @@ const defaultHtml = `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta
       name="apple-itunes-app"
-      content="app-id=myAppID, app-argument=#{{deepLinkUrl}}"
+      content="app-id=#{{appID}}, app-argument=#{{deepLinkUrl}}"
     />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="white" />
