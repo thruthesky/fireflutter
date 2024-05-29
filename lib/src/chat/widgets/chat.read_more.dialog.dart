386dbc78db83907907d97449dea10d72ba4bbc4f
Christian Bucal <withcenter-dev2@github.com>
2024-05-29 17:59:35 +0800

    scroll view were having problem in Ignore pointer

--- lib/src/chat/widgets/chat.read_more.dialog.dart
+++ lib/src/chat/widgets/chat.read_more.dialog.dart
@@ -11,52 +11,47 @@ class ReadMoreDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IgnorePointer(
-      ignoring: false,
-      child: AlertDialog(
-        title: SingleChildScrollView(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+    return AlertDialog(
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          UserAvatar(
+            key: ValueKey(message.key),
+            uid: message.uid!,
+            cacheId: message.uid,
+            size: 30,
+            radius: 12,
+          ),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              UserAvatar(
-                key: ValueKey(message.key),
-                uid: message.uid!,
-                cacheId: message.uid,
-                size: 30,
-                radius: 12,
-              ),
-              const SizedBox(width: 8),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  DateTimeShort(stamp: message.createdAt ?? 0),
-                  const SizedBox(width: 4),
-                  ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width * 0.2,
-                    ),
-                    child: UserDisplayName(
-                      uid: message.uid,
-                      cacheId: 'chatRoom',
-                      style: Theme.of(context).textTheme.labelSmall,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-              const Spacer(),
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                icon: const Icon(Icons.close),
+              DateTimeShort(stamp: message.createdAt ?? 0),
+              const SizedBox(width: 4),
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.2,
+                ),
+                child: UserDisplayName(
+                  uid: message.uid,
+                  cacheId: 'chatRoom',
+                  style: Theme.of(context).textTheme.labelSmall,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
             ],
           ),
-        ),
-        content: Text(message.text!),
+          const Spacer(),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.close),
+          ),
+        ],
       ),
+      content: SingleChildScrollView(child: Text(message.text!)),
     );
   }
 }
