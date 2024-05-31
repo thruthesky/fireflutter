14abf16f963b809711a7203ebd0612f0ee99e797
withcenterdev5 <withcenter.dev5@gmail.com>
2024-05-31 13:34:42 +0800

    adding test case

---
+++ example/integration_test/app_test.dart
@@ -0,0 +1,28 @@
+import 'package:example/main.chat.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // ···
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      // Load app widget.
+      await tester.pumpWidget(const ChatApp());
+
+      // // Verify the counter starts at 0.
+      // expect(find.text('0'), findsOneWidget);
+
+      // // Finds the floating action button to tap on.
+      // final fab = find.byKey(const ValueKey('increment'));
+
+      // // Emulate a tap on the floating action button.
+      // await tester.tap(fab);
+
+      // // Trigger a frame.
+      // await tester.pumpAndSettle();
+
+      // // Verify the counter increments by 1.
+      expect(find.text('Entry Screen'), findsOneWidget);
+    });
+  });
+}
