a08aa6010633098cc8d2b025714b341bc68666ad
Christian Bucal <withcenter-dev2@github.com>
2024-06-10 10:22:00 +0800

    removed unneeded import in chat room body

--- lib/src/chat/widgets/chat.room_body.dart
+++ lib/src/chat/widgets/chat.room_body.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:fireflutter/fireflutter.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
