b9e9cc68c192ce078a3e40cb64003387a7537e0d
JaeHo Song <thruthesky@gmail.com>
2024-06-13 17:57:17 +0800

    update on forum post widget

--- lib/src/forum/post/widgets/post.list_tile.dart
+++ lib/src/forum/post/widgets/post.list_tile.dart
@@ -1,15 +1,32 @@
 import 'package:fireflutter/fireflutter.dart';
 import 'package:flutter/material.dart';
 
+/// A list tile for the post list view.
+///
+/// Use this widget to display a post as a ListTile.
 class PostListTile extends StatelessWidget {
-  const PostListTile({super.key, required this.post});
+  const PostListTile({
+    super.key,
+    required this.post,
+    this.tight = false,
+  });
 
   final Post post;
+  final bool tight;
+
+  /// A small (tight) version of the post list tile.
+
+  PostListTile.small({
+    super.key,
+    required this.post,
+  }) : tight = true;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: UserAvatar(
+        size: tight ? 32 : 48,
+        radius: tight ? 14 : 20,
         uid: post.uid,
         onTap: () => UserService.instance.showPublicProfileScreen(
           context: context,
@@ -24,7 +41,7 @@ class PostListTile extends StatelessWidget {
       ),
       subtitle: Text(
         post.content.upTo(60).sanitize,
-        maxLines: 2,
+        maxLines: tight ? 1 : 2,
         overflow: TextOverflow.ellipsis,
       ),
       trailing: Column(
