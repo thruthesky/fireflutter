560b44fbbeb175459d8574223dc1428fd3ef261b
Noe <withcenter.dev4@gmail.com>
2024-06-13 14:48:21 +0800

    revert code .

--- example/lib/screens/home/home.screen.dart
+++ example/lib/screens/home/home.screen.dart
@@ -1,6 +1,5 @@
 import 'package:example/screens/buyandsell/buyandsell.screen.dart';
 import 'package:example/screens/chat/chat.screen.dart';
-import 'package:example/screens/chat/custom_chat.screen.dart';
 import 'package:example/screens/chat/open_chat.screen.dart';
 import 'package:example/screens/entry/entry.screen.dart';
 import 'package:example/screens/forum/forum.screen.dart';
@@ -97,12 +96,6 @@ class _HomeScreenState extends State<HomeScreen> {
                         onPressed: () => AdminService.instance
                             .showDashboard(context: context),
                         child: const Text('Admin dashboard'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          context.push(CustomChatScreen.routeName);
-                        },
-                        child: const Text('Custom Chat'),
                       ),
                     ],
                   ),
