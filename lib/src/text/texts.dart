51179104c6403dacff016dbe5df55b44bcd6b13f
Christian Bucal <withcenter-dev2@github.com>
2024-06-19 15:53:33 +0800

    translations in text

--- lib/src/text/texts.dart
+++ lib/src/text/texts.dart
@@ -325,6 +325,11 @@ class T {
     'ko': '초기화',
   };
 
+  static Json age = {
+    'en': 'Age',
+    'ko': '나이',
+  };
+
   static Json info = {
     'en': 'Info',
     'ko': '정보',
@@ -1448,6 +1453,11 @@ class T {
   static const Json female = {
     'en': 'Female',
     'ko': '여자',
+  };
+
+  static const Json noUserFound = {
+    'en': 'No user found',
+    'ko': '사용자를 찾을 수 없습니다.',
   };
 
   // static const String nationality = 'Nationality';
