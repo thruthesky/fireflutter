c7f799ca836fce0755b9f13626974cd512ed3df7
Christian Bucal <withcenter-dev2@github.com>
2024-06-20 17:10:57 +0800

    reverted user model, removed the customData

--- lib/src/user/user.model.dart
+++ lib/src/user/user.model.dart
@@ -506,7 +506,6 @@ class User {
       if (longitude != null) 'longitude': longitude,
       if (languageCode != null) 'languageCode': languageCode,
       if (ping != null) 'ping': ping,
-      if (customData != null) ...customData,
     };
     if (data.isEmpty) {
       return this;
