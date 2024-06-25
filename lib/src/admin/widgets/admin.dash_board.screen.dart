8750093a57a9c23951cdcde16d01490ac84d9d2a
withcenterdev5 <withcenter.dev5@gmail.com>
2024-06-25 10:44:16 +0800

    conflict fixed

--- lib/src/admin/widgets/admin.dash_board.screen.dart
+++ lib/src/admin/widgets/admin.dash_board.screen.dart
@@ -15,7 +15,6 @@ class _AdminDashBoardScreenState extends State<AdminDashBoardScreen> {
       appBar: AppBar(
         title: const Text('Admin DashBoard'),
       ),
-<<<<<<< HEAD
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: SingleChildScrollView(
@@ -84,68 +83,6 @@ class _AdminDashBoardScreenState extends State<AdminDashBoardScreen> {
                 title: const Text('Meetup Management'),
               ),
             ],
-=======
-      body: Column(
-        children: [
-          const Text('Chat'),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Wrap(
-              spacing: 8,
-              runSpacing: 8,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    AdminService.instance.showUserList(context: context);
-                  },
-                  child: const Text('전체 사용자 목록'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('신고목록'),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    final re = await confirm(
-                        context: context,
-                        title: T.confirmSendTestMessageToAllUsers.tr,
-                        message: T.confirmSendTestMessageToAllUsersMessage.tr);
-                    if (re != true) return;
-                    dog('send push messages to all users');
-                    MessagingService.instance.sendAll(
-                      title: 'send all test - ${DateTime.now()}',
-                      body: 'this is the content of the message',
-                    );
-                  },
-                  child: const Text('Send push messages to all users'),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    final re = await confirm(
-                        context: context,
-                        title: T.confirmMirrorUserDataToFirestore.tr,
-                        message: T.confirmMirrorUserDataToFirestoreMessage.tr);
-                    if (re != true) return;
-
-                    final result = await AdminService.instance
-                        .mirrorUserBackfillRtdbToFirestore();
-                    final response = result.data;
-                    print(response.toString());
-                  },
-                  child: const Text('Mirror User Data To Firestore'),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    showGeneralDialog(
-                        context: context,
-                        pageBuilder: (_, __, ___) =>
-                            const AdminMeetupListScreen());
-                  },
-                  child: const Text('Meetup Management'),
-                ),
-              ],
-            ),
->>>>>>> main
           ),
         ),
       ),
