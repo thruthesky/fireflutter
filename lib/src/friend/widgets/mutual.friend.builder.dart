0ddd1ac7d432394734f62a6476434f16abcf34e6
Christian Bucal (Dev2) <withcenter.dev2@gmail.com>
2024-06-28 16:35:47 +0800

    added yes no in Mutual Friend Builder

--- lib/src/friend/widgets/mutual.friend.builder.dart
+++ lib/src/friend/widgets/mutual.friend.builder.dart
@@ -1,14 +1,17 @@
 import 'package:fireflutter/fireflutter.dart';
 import 'package:flutter/material.dart';
 
-// TODO ask for help how is it going to be used
 class MutualFriendBuilder extends StatelessWidget {
   const MutualFriendBuilder({
     super.key,
     required this.friend,
+    this.yes,
+    this.no,
   });
 
   final Friend friend;
+  final Widget? yes;
+  final Widget? no;
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +19,9 @@ class MutualFriendBuilder extends StatelessWidget {
       ref: Friend.myListRef.child(friend.uid),
       builder: (myFriendData) {
         if (myFriendData == null) {
-          return const Text("Not Mutual");
+          return no ?? const SizedBox.shrink();
         } else {
-          return const Text("Mutual");
+          return yes ?? const SizedBox.shrink();
         }
       },
     );
